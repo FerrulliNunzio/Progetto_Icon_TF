8221764f33320aabfe8a4f59843a118cc8f1f99e
FerrulliNunzio <n.ferrulli5@studenti.uniba.it>
2022-08-12 19:32:40 +0200

    Aggiunti i commenti alla classe Graph e alla classe Node

--- Map/Graph.py
+++ Map/Graph.py
@@ -1,14 +1,63 @@
 from Map.Node import Node
 
+'''
 
+Classe che implementa la realizzazione della
+classe Graph
+
+'''
 class Graph:
+    '''
+
+    Comportamento:
+        Il metodo "__init__" viene chiamato automaticamente appena andiamo
+        a creare un oggetto e ci permette di inizializzare gli attributi della classe.
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output
+        Inizializzazione attributi:
+        self.node;
+        self.connections;
+
+    '''
     def __init__(self):
         self.node = []
         self.connections = {}
 
+    '''
+
+    Comportamento:
+        Il metodo "add_node" aggiunge un nodo al membro self.node
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node: nodo che sarà aggiunto al membro self.node
+
+    Output
+        Valori del membro self.node aggiornato
+
+    '''
     def add_node(self, node):
         self.node.append(node)
 
+    '''
+
+    Comportamento:
+        Il metodo "__oriented_graph_connection" connette due nodi con arco avente
+        peso passato come argomento in un grafo orientato
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node1: nodo da connettere con l'altro nodo passato come argomento
+        node2: nodo da connettere con l'altro nodo passato come argomento
+        weight: peso dell'arco con cui connettere i nodi
+
+    Output
+        Connessione di node1 a node2 in un grafo orientato
+
+    '''
     def __oriented_graph_connection(self, node1, node2, weight):
         if node1 in self.connections:
             all_nodes = self.connections.get(node1)
@@ -16,6 +65,22 @@ class Graph:
         else:
             self.connections[node1] = {node2: weight}
 
+    '''
+
+    Comportamento:
+        Il metodo "__undirected_graph_connection" connette due nodi con arco avente
+        peso passato come argomento in un grafo non orientato
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node1: nodo da connettere con l'altro nodo passato come argomento
+        node2: nodo da connettere con l'altro nodo passato come argomento
+        weight: peso dell'arco con cui connettere i nodi
+
+    Output
+        Connessione di node1 a node2 in un grafo non orientato
+
+    '''
     def __undirected_graph_connection(self, node1, node2, weight):
         if node2 in self.connections:
             all_nodes = self.connections.get(node2)
@@ -23,6 +88,22 @@ class Graph:
         else:
             self.connections[node2] = {node1: weight}
 
+    '''
+
+    Comportamento:
+        Il metodo "connect" connette due nodi con arco avente
+        peso passato come argomento
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node1: nodo da connettere con l'altro nodo passato come argomento
+        node2: nodo da connettere con l'altro nodo passato come argomento
+        weight: peso dell'arco con cui connettere i nodi
+
+    Output
+        Connessione di node1 a node2 in un grafo
+
+    '''
     def connect(self, node1, node2, weight):
         if type(weight).__name__ != "int" and type(weight).__name__ != "float":
             raise Exception("Peso non valido".format(type(weight).__name__))
@@ -34,6 +115,20 @@ class Graph:
         else:
             raise Exception("L'arco non è valido".format(node1, node2))
 
+    '''
+
+    Comportamento:
+        La funzione "path_weight" restituisce peso di arco tra due nodi
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node1: nodo connesso con l'altro nodo passato come argomento
+        node2: nodo connesso con l'altro nodo passato come argomento
+
+    Output
+        Valore del peso dell'arco dato dalla connessione fra node1 e node2
+
+    '''
     def path_weight(self, node1, node2):
         if node1 in self.connection and node2 in self.connection:
             support_connection = self.connections.get(node1)
@@ -44,6 +139,19 @@ class Graph:
         else:
             raise Exception("L'arco non è valido".format(node1, node2))
 
+    '''
+
+    Comportamento:
+        La funzione "connection" restituisce una lista di nodi adiacienti a node
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node: nodo da cui prendere i nodi adiacenti ad esso
+
+    Output
+        Lista di nodi adiacenti a node
+
+    '''
     def connection(self, node):
         connection_result = []
         if node in self.connections:
@@ -52,16 +160,72 @@ class Graph:
 
         return connection_result
 
+    '''
+
+        Comportamento:
+            La funzione "nodes" restituisce i nodi presenti nel membro self.node
+
+        Input:
+            self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+        Output
+            nodi presenti in self.node
+
+        '''
     def nodes(self):
         return self.node
 
+    '''
+
+    Comportamento:
+        La funzione "__str__" restituisce una rappresentazione dell'oggetto
+        sotto forma di stringa
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output:
+        Valore del membro self.__class__.__name__
+
+    '''
     def __str__(self):
         return self.__class__.__name__
 
+    '''
+
+    Comportamento:
+        La funzione statica "euristic" calcola il valore assoluto di un numero,
+        eliminando il segno negativo
+
+    Input:
+        node1: nodo con il quale calcolare il valore assoluto
+        node2: nodo con il quale calcolare il valore assoluto
+    
+    Output:
+        Valore assoluto tra la differenza del membro x dei nodi node1
+        e node2 sommati alla differenza del membro y dei nodi node1 e node2
+
+    '''
     @staticmethod
     def euristic(node1, node2):
         return abs(node1.get_x() - node2.get_x()) + abs(node1.get_y() - node2.get_y())
 
+    '''
+
+    Comportamento:
+        La funzione "min_search" data una lista restituisce il nodo
+        avente il parametro totalDistanceValue minimo.
+        (euristica + distanza stimata)
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento       
+        list: lista dalla quale trovare il nodo avente il parametro 
+              totalDistanceValue minimo
+        
+    Output:
+        Valore minimo nei nodi del parametro totalDistanceValue
+
+    '''
     def min_search(self, list: list()):
         min = Node("", 100, 100)
         min.set_real_distance_value(9999)
@@ -74,6 +238,21 @@ class Graph:
 
         return min
 
+    '''
+
+    Comportamento:
+        La funzione "a_strar" calcola il percorso migliore per arrivare
+        da un nodo iniziale (start) a un nodo obiettivo (goal)
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento       
+        start: nodo iniziale da cui raggiungere l'obiettivo (goal)
+        goal: nodo obiettivo da raggiungere partendo da un nodo iniziale (start)
+
+    Output:
+        percorso minimo tra il nodo iniziale (start) e il nodo obiettivo (goal)
+
+    '''
     def a_star(self, start, goal):
 
         if start not in self.connections or goal not in self.connections:
@@ -106,6 +285,21 @@ class Graph:
                 successor.set_parent(current)
             closed_list.append(current)
 
+    '''
+
+    Comportamento:
+        La funzione "print_path" stampa il percorso trovato per arrivare
+        al nodo destinazione
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento       
+        node: nodo da cui stampae il path
+        path: percorso del nodo
+
+    Output:
+        stampa del percorso del nodo
+
+    '''
     def print_path(self, node, path):
         node: Node()
 
--- Map/Node.py
+++ Map/Node.py
@@ -1,36 +1,176 @@
+'''
+
+Classe che implementa la realizzazione della
+classe Node
+
+'''
 class Node:
+
+    # Variabile che contiene la somma del valore della distanza Euristica e il valore della distanza effettiva:
+    # Distanza totale = Distanza Euristica + Distanza Effettiva
     totalDistanceValue: int = 0
+
+    # Variabile che contiene il valore della Distanza Effettiva
     realDistanceValue: int = 0
+
+    # Variabile che contiene il valore della Distanza Euristica
     heuristicDistanceValue: int = 0
 
+    '''
+    
+    Comportamento:
+        Il metodo "__init__" viene chiamato automaticamente appena andiamo
+        a creare un oggetto e ci permette di inizializzare gli attributi della classe.
+    
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        value: variabile che inizializzerà il valore di self.value
+        x: variabile che inizializzerà il valore di self.x
+        y: variabile che inizializzerà il valore di self.y
+        
+    Output
+        Inizializzazione attributi:
+        self.value;
+        self.x;
+        self.y
+        
+    '''
     def __init__(self, value, x, y):
         self.parent = None
         self.value = value
         self.x = x
         self.y = y
 
+    '''
+    
+    Comportamento:
+        Il metodo "set_parent" imposta il valore del nodo genitore
+        del nodo corrente nel grafo
+    
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        parent_node: variabile contente il valore che sarà assegnato
+                     alla variabile self.parent_node
+                     
+    Output:
+        Valore aggiornato della variabile self.parent_node
+    
+    '''
     def set_parent(self, parent_node):
         self.parent = parent_node
 
+    '''
+    
+    Comportamento:
+        Il metodo "set_real_distance_value" imposta il valore della variabile realDistanceValue
+        e calcola il valore della variabile totalDistanceValue
+    
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        distance_value: variabile contente il valore che sarà assegnato
+                        alla variabile self.realDistanceValue
+                     
+    Output:
+        Valore aggiornato della variabile self.realDistanceValue
+    
+    '''
     def set_real_distance_value(self, distance_value):
         self.realDistanceValue = distance_value
         self.set_total_distance_value()
 
+    '''
+
+    Comportamento:
+        Il metodo "set_heuristic_distance_value" imposta il valore della variabile heuristicDistanceValue
+        e calcola il valore della variabile totalDistanceValue
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        distance_value: variabile contente il valore che sarà assegnato
+                        alla variabile self.heuristicDistanceValue
+
+    Output:
+        Valore aggiornato della variabile self.heuristicDistanceValue
+
+    '''
     def set_heuristic_distance_value(self, distance_value):
         self.heuristicDistanceValue = distance_value
         self.set_total_distance_value()
 
+    '''
+
+    Comportamento:
+        Il metodo "set_total_distance_value" calcola il valore della variabile totalDistanceValue
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output:
+        Valore aggiornato della variabile self.totalDistanceValue
+
+    '''
     def set_total_distance_value(self):
         self.totalDistanceValue = self.realDistanceValue + self.heuristicDistanceValue
 
+    '''
+    
+    Comportamento:
+        La funzione "get_x" restituisce il valore del membro self.x
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output:
+        Valore del self.x
+
+    '''
     def get_x(self):
         return self.x
 
+    '''
+
+    Comportamento:
+        La funzione "get_y" restituisce il valore del membro self.y
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output:
+        Valore del self.y
+
+    '''
     def get_y(self):
         return self.y
 
+    '''
+
+    Comportamento:
+        La funzione "is_same" di tipo booleano restituisce true se il valore del
+        membro self.value è uguale al valore della valore passato nella funzione.
+        Restituisce false altrimenti
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+        node: variabile che andrà confrontata con l'istanza dell'oggetto
+    Output:
+        true se slef.value == node.value, false altrimenti
+
+    '''
     def is_same(self, node):
         return self.value == node.value
 
+    '''
+
+    Comportamento:
+        Funzione "__str__" che restituisce una rappresentazione dell'oggetto
+        sotto forma di stringa
+
+    Input:
+        self: rappresenta l'istanza dell'oggetto a cui si fa riferimento
+
+    Output:
+        Valore del membro self.x
+
+    '''
     def __str__(self):
         return str(self.value)
