61f2566306046e491d942e59b1a5d90aaed6c23a
FerrulliNunzio <n.ferrulli5@studenti.uniba.it>
2022-11-06 18:20:11 +0100

    Corretti regressore e documentazione

--- Classifier.py
+++ Classifier.py
@@ -27,7 +27,7 @@ def set_training(balance_data, test):
 def train_using_entropy(X_train, y_train):
     clf_entropy = DecisionTreeRegressor(
         criterion="friedman_mse", random_state=0,
-        max_depth=4, max_features='sqrt')
+        max_depth=4, max_features='sqrt', min_samples_split=5)
     clf_entropy.fit(X_train, y_train)
     return clf_entropy
 
@@ -55,29 +55,32 @@ def cross_validation():
 
     clf = DecisionTreeRegressor(
         criterion="friedman_mse", random_state=0,
-        max_depth=4, max_features='sqrt')
+        max_depth=4, max_features='sqrt', min_samples_split=5)
 
     score = cross_val_score(clf, X_train, y_train, cv=5)
     accurancy = numpy.round(score.mean(),2)
     deviation = numpy.round(score.std(),2)
+    print("Risultati cross validation:\nAccurancy: " + str(accurancy) + "\nDeviation: " + str(deviation))
 
 def metrix_test():
     data = importdata("DataSet.csv")
     X = data.values[:, 0:5]
     Y = data.values[:, 5]
 
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, Y, test_size=0.25)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
 
     clf = DecisionTreeRegressor(
         criterion="friedman_mse", random_state=0,
-        max_depth=4, max_features='sqrt')
+        max_depth=4, max_features='sqrt', min_samples_split=5)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test, clf)
 
-    mean = mean_squared_error(y_test, y_pred)
-    score = r2_score(y_test, y_pred)
+    mean = numpy.round(mean_squared_error(y_test, y_pred), 2)
+    score = numpy.round(r2_score(y_test, y_pred), 2)
+    print("Risultati delle metriche:\nMean squared error: " + str(mean) + "\nr-quadro score: " + str(score))
 
 def test():
+    print("-----------Test classificatore-----------")
     cross_validation()
     metrix_test()
+    print("---------------------------------------- ")
